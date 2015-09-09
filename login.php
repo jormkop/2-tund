8b0ffde87ba4f976fd4f7ccdee66edfa85d1d723
Jorma Kõpp <jormkop@greeny.cs.tlu.ee>
2015-09-09 12:38:38 +0300

    ok siis

---
+++ login.php
@@ -0,0 +1,19 @@
+<?php
+
+	// LOGIN.PHP
+	
+?>
+<html>
+<head>
+	<title>Login page</title>
+</head>
+<body>
+	<h2>Log in</h2>
+	
+		<input type="email" placeholder="Email"> <br><br>
+		<input type="password" placeholder="Parool"> <br><br>
+		<input type="submit" value="Press here">
+	
+	<h2>Create user</h2>
+</body>
+</html>
