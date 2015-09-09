1a6c55d588a4c7342b6e5301d30966544b0ec5a2
Jorma Kõpp <jormkop@greeny.cs.tlu.ee>
2015-09-09 11:33:46 +0300

    updated

--- Helloworld.php
+++ Helloworld.php
@@ -5,3 +5,18 @@
 	
 	
 ?>
+<br>
+<?php
+	$age = 4;
+	$sex = "5-6 times a week"
+	
+	//kell oli pool 3 kui mati kooli jõudis
+	
+	if(age <18) {
+		//tõene
+		echo "alakas"
+	} else {
+		//väär
+		echo "täisealine"
+		}
+?>
