96c2c58251740a5af437cecfe724a17f149d217c
Jorma Kõpp <jormkop@greeny.cs.tlu.ee>
2015-09-09 13:25:38 +0300

    väike parandus

--- login.php
+++ login.php
@@ -1,7 +1,32 @@
 <?php
 
 	// LOGIN.PHP
+	echo $_POST["email"];
 	
+	//keegi näppis mu nuppu
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+		
+		//echo "   Näpud eemale!";
+		//kontrollin et email poleks tühi
+		$email_error = " ";
+		$password_error = " ";
+		
+		if ( empty($_POST["email"]) ) {
+			$email_error = "See väli on kohustuslik";
+		}
+		
+		//kontrollin, et parool ei ole tühi
+		if ( empty($_POST["password"]) ) {
+			$password_error = "See väli on kohustuslik";
+		} else {
+			
+			//kui oleme siia jõudnud siis parool ei ole tühi
+			if(strlen($_POST["password"]) < 8) {
+				
+				$password_error = "peab olema vähemalt 8 tähemärki pikk";
+			}		
+		}
+	}
 ?>
 <html>
 <head>
@@ -9,10 +34,11 @@
 </head>
 <body>
 	<h2>Log in</h2>
-	
-		<input type="email" placeholder="Email"> <br><br>
-		<input type="password" placeholder="Parool"> <br><br>
-		<input type="submit" value="Press here">
+		<form action="login.php" method="post" >
+			<input name="email" type="email" placeholder="Email"> <?php echo $email_error ?> <br><br>
+			<input name="pass" type="password" placeholder="Parool"> <?php echo $password_error ?> <br><br>
+			<input type="submit" value="Press here">
+		</form>
 	
 	<h2>Create user</h2>
 </body>
